46d3d53a969335bd3bfa83666dd3e3d4925afe5f
German Romarion <grromarion@hotmail.com>
2014-08-24 17:53:23 -0300

    Finished with docx report.

--- tp1.pod/src/main/java/ar/edu/itba/tp1/pod/ej3/ThreadFactory.java
+++ tp1.pod/src/main/java/ar/edu/itba/tp1/pod/ej3/ThreadFactory.java
@@ -1,28 +1,29 @@
 package ar.edu.itba.tp1.pod.ej3;
 
-import java.util.Stack;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class ThreadFactory {
 
-	private static Stack<String> strings = new Stack<String>();
-	
+	private static ConcurrentLinkedQueue<String> strings = new ConcurrentLinkedQueue<String>();
+
 	public static String getString() {
-		if (strings.empty())
+		if (strings.isEmpty())
 			return null;
-		return strings.pop();			
+		System.out.println(strings.size());
+		return strings.poll();
 	}
-	
+
 	public static void addString(String string) {
-		strings.push(string);
+		strings.add(string);
 	}
-	
+
 	public static ProducerThread[] createProducers(int amount) {
 		ProducerThread[] ans = new ProducerThread[amount];
 		for (int i = 0; i < amount; i++)
 			ans[i] = new ProducerThread();
 		return ans;
 	}
-	
+
 	public static ConsumerThread[] createConsumers(int amount) {
 		ConsumerThread[] ans = new ConsumerThread[amount];
 		for (int i = 0; i < amount; i++)
